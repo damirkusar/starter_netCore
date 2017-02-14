583b5638d2b0d8fed19718196a6e5639414c49a2
Damir Kusar <damir@kusar.ch>
2017-02-14 14:19:03 +0100

    Added .net core test project.

---
+++ WebApp.Test/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace WebApp.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase(1, 1, 2)]
+        public void PassingTest(int x, int y, int expected)
+        {
+            Assert.That(this.Add(x, y), Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2, 2)]
+        public void FailingTest(int x, int y, int expected)
+        {
+            Assert.That(this.Add(x, y), Is.EqualTo(expected));
+        }
+
+        private int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
