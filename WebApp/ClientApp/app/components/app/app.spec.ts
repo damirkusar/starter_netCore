9cdf42dda498396cc9d88db733bc1b3a77ecae51
Damir Kusar <damir@kusar.ch>
2017-03-13 10:21:22 +0100

    Fixed test

--- WebApp/ClientApp/app/components/app/app.spec.ts
+++ WebApp/ClientApp/app/components/app/app.spec.ts
@@ -2,23 +2,18 @@ import { } from 'jasmine';
 import { MainSpec } from '../../main.spec';
 import { AppComponent } from './app.component';
 
-import { NavigationComponent } from '../../modules/navigation/navigation/navigation.component';
-import { NavMenuTopComponent } from '../../modules/navigation/components/navmenu-top/navmenu-top.component';
-import { NavMenuElementsLocalizationComponent } from '../../modules/navigation/components/navmenu-elements-localization/navmenu-elements-localization.component';
-import { NavMenuElementsLoginComponent } from '../../modules/navigation/components/navmenu-elements-login/navmenu-elements-login.component';
-import { NavMenuElementsNotificationComponent } from '../../modules/navigation/components/navmenu-elements-notification/navmenu-elements-notification.component';
+import { NavigationModule } from '../../modules/navigation/navigation.module';
+
 
 describe('AppComponent', () => {
     beforeEach(() => {
         this.spec = new MainSpec();
         this.spec.init(AppComponent,
+            [],
             [
-                NavigationComponent,
-                NavMenuTopComponent,
-                NavMenuElementsLocalizationComponent,
-                NavMenuElementsLoginComponent,
-                NavMenuElementsNotificationComponent
-            ]);
+                NavigationModule
+            ]
+        );
     });
 
     afterEach(() => {
