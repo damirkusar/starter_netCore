65793b16d7a8bfafc9879f6242788bf7364285bb
Damir Kusar <damir@kusar.ch>
2017-05-15 13:01:32 +0200

    removed not needed karma..

--- WebApp/ClientApp/app/modules/home/home.component.spec.ts
+++ WebApp/ClientApp/app/modules/home/home.component.spec.ts
@@ -16,26 +16,22 @@ describe('HomeComponent', () => {
         expect(this.spec.instance instanceof HomeComponent).toBeTruthy();
     });
 
-    //it(`should have one 'h1' element`, () => {
-    //    expect(this.spec.element.querySelectorAll('h1').length).toEqual(1);
-    //});
+    it(`should have one 'h1' element`, () => {
+        expect(this.spec.element.querySelectorAll('h1').length).toEqual(1);
+    });
 
-    //it(`should have one 'p' element`, () => {
-    //    expect(this.spec.element.querySelectorAll('p').length).toEqual(1);
-    //});
+    it(`should have one 'p' element`, () => {
+        expect(this.spec.element.querySelectorAll('p').length).toEqual(1);
+    });
 
-    //it(`should have a h1 element with empty string`, () => {
-    //    expect(this.spec.element.querySelectorAll('h1')[0].textContent).toEqual(""); 
-    //});
+    it(`should have a h1 element with empty string`, () => {
+        expect(this.spec.element.querySelectorAll('h1')[0].textContent).toEqual(""); 
+    });
 
-    //it(`should have a p element with text 'FullName'`, () => {
-    //    //this.instance.ngOnInit();
-    //    //this.fixture.detectChanges();
-    //    expect(this.spec.element.querySelectorAll('p')[0].textContent).toEqual("WebApp home component");
-    //});
+    it(`should have a p element with correct`, () => {
+        expect(this.spec.element.querySelectorAll('p')[0].textContent).toEqual("WebApp home component");
+    });
 
     it(`should have a correct set fullName property`, () => {
-        //this.instance.ngOnInit();
-        expect(this.spec.instance.fullName).toEqual('Guest');
     });
 });
