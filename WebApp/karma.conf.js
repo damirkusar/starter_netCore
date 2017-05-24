a26a406ccc9c1574e7691f6e0eed3c78c1267383
Damir Kusar <damir@kusar.ch>
2017-05-24 20:51:06 +0200

    added karma back, so user can decide to use jest, karma or both..

---
+++ WebApp/karma.conf.js
@@ -0,0 +1,71 @@
+var WebpackConfig = require('./webpack.config.spec');
+
+module.exports = function (config) {
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+        // list of files / patterns to load in the browser
+        files: [
+            { pattern: './karma.testshim.config.js', watched: false }
+        ],
+        // list of files to exclude
+        exclude: [
+        ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            './karma.testshim.config.js': ['webpack'] //, 'sourcemap']
+        },
+        webpack: WebpackConfig,
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+        webpackServer: {
+            noInfo: true
+        },
+        phantomjsLauncher: {
+            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom) 
+            exitOnResourceError: true
+        },
+        plugins: [
+            'karma-jasmine',
+            'karma-sourcemap-loader',
+            'karma-webpack',
+            'karma-remap-istanbul',
+            'karma-chrome-launcher',
+            'karma-jasmine-html-reporter'
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible
+        },
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'kjhtml'],
+        // web server port
+        port: 9877,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_WARN,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: 1, //Infinity,
+        captureTimeout: 210000,
+        browserDisconnectTolerance: 3, //this one helps
+        browserDisconnectTimeout: 210000,
+        browserNoActivityTimeout: 210000
+    });
+}
