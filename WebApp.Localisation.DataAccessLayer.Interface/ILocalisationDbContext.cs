e60ec6c67c363c3b1e8a7ca1db9950b306e8a693
Damir Kusar <damir@kusar.ch>
2017-11-01 20:59:57 +0100

    removed not needed project.

--- WebApp.Localisation.DataAccessLayer.Interface/ILocalisationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace WebApp.Localisation.DataAccessLayer.Interface
-{
-    public interface ILocalisationDbContext
-    {
-        DbSet<Models.Localisation> Localisations { get; set; }
-    }
-}
