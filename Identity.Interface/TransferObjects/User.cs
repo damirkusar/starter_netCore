f1e0823be137b6eb5cf3c59a52a6c134b331f7de
Damir Kusar <damir@kusar.ch>
2017-12-29 17:55:39 +0100

    removed Password from dto

--- Identity.Interface/TransferObjects/User.cs
+++ Identity.Interface/TransferObjects/User.cs
@@ -9,6 +9,5 @@ namespace Identity.Interface.TransferObjects
         public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
